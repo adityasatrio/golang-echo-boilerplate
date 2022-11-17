bae98cee973a86d17d573091d19e3dea2de3037b
adityasatrio <aditya.satrio.nugroho@gmail.com>
2022-11-17 20:10:39 +0700

    add validator

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
 	"myapp/commons/middlewares"
 	"myapp/modules/system_parameter"
+	"net/http"
 )
+
+type CustomValidator struct {
+	validator *validator.Validate
+}
+
+func (cv *CustomValidator) Validate(i interface{}) error {
+	if err := cv.validator.Struct(i); err != nil {
+		// Optionally, you could return the error to give each route more control over the status code
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return nil
+}
 
 func main() {
 	e := echo.New()
+	e.Validator = &CustomValidator{validator: validator.New()}
 
 	//add middlewares
 	middlewares.InitMiddlewares(e)
