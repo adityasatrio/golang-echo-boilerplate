8d8d9a522caa2f18da5033f1545dca171826ce77
adityasatrio <aditya.satrio.nugroho@gmail.com>
2023-03-18 16:16:59 +0700

    adding unfinished unit test - mock service

--- internal/applications/hello_worlds/controller/hello_worlds_controller_test.go
+++ internal/applications/hello_worlds/controller/hello_worlds_controller_test.go
@@ -1,3 +1,49 @@
 package controller
 
-//todo : create test
+import (
+	"github.com/labstack/echo/v4"
+	"myapp/internal/applications/hello_worlds/service"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type MockHelloWorldService struct {
+	service.HelloWorldsService
+
+	//mockNameFunction - func - return value of service
+	MockHello func() (string, error)
+}
+
+func (mock *MockHelloWorldService) Hello() (string, error) {
+	return mock.MockHello()
+}
+
+/* enable when already use logger
+func setUp_author_test() func() {
+	logger.New()
+
+	return func() {
+		logger.Sync()
+		logger.Delete()
+	}
+}*/
+
+func TestHello(t *testing.T) {
+	//	defer setUp_author_test()()
+
+	mockService := &service.
+	//setup
+	e := echo.New()
+	request := httptest.NewRequest(http.MethodGet, "/hello", nil)
+	request.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+
+	record := httptest.NewRecorder()
+	c := e.NewContext(request, record)
+
+	//mockService := &service.MockHelloWorldsService{}
+	//mockService.On("Hello", context.Background(), "hello from controller -", "").Return("Hello World", nil)
+
+	controller := &HelloWorldsController{}
+
+}
