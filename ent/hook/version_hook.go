88eff576dbe13ad64cce13700cef96dee41a39a7
adityasatrio <aditya.satrio.nugroho@gmail.com>
2023-06-06 23:41:55 +0700

    optimize import and polishing code

--- ent/hook/version_hook.go
+++ ent/hook/version_hook.go
@@ -2,6 +2,7 @@ package hook
 
 import (
 	"context"
+	"fmt"
 	"github.com/labstack/gommon/log"
 	"myapp/ent"
 	"time"
@@ -18,30 +19,15 @@ func VersionHook() ent.Hook {
 					// An error is returned, if the field is not defined in
 					// the schema, or if the type mismatch the field type.
 					log.Errorf("FAILED INIT VERSION Op=%s\tType=%s\tTime=%s\tConcreteType=%T\n", m.Op(), m.Type(), time.Since(start), m)
+					return nil, fmt.Errorf("failed to set version: %w", err)
 				}
 			}
 
-			v, _ := next.Mutate(ctx, m)
+			v, err := next.Mutate(ctx, m)
+			if err != nil {
+				return nil, fmt.Errorf("mutation failed: %w", err)
+			}
 			return v, nil
 		})
 	}
 }
-
-// Option 2: use the generic ent.Mutation interface.
-//dbConnection.Use(func(next ent.Mutator) ent.Mutator {
-//	return ent.MutateFunc(func(ctx context.Context, m ent.Mutation) (ent.Value, error) {
-//		if m.Op().Is(1) || m.Op().Is(2) || m.Op().Is(3) {
-//			start := time.Now()
-//			err := m.SetField("version", time.Now().UnixNano())
-//			if err != nil {
-//				log.Errorf("FAILED INIT VERSION Op=%s\tType=%s\tTime=%s\tConcreteType=%T\n", m.Op(), m.Type(), time.Since(start), m)
-//			}
-//		}
-//
-//		//if err := m.SetField("name", "Ariel Mashraki"); err != nil {
-//		//	// An error is returned, if the field is not defined in
-//		//	// the schema, or if the type mismatch the field type.
-//		//}
-//		return next.Mutate(ctx, m)
-//	})
-//})
